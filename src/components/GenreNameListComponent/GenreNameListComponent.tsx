7677a238d2b15105c85bdad56f8278dd55ea5d00
Veronika-Bortkova <shishiga79@gmail.com>
2026-09-15 23:54:33 +0300

    moviesCard PageGenre+++

--- src/components/GenreNameListComponent/GenreNameListComponent.tsx
+++ src/components/GenreNameListComponent/GenreNameListComponent.tsx
@@ -2,6 +2,7 @@ import React, {FC} from 'react';
 import {getGenres} from "@/src/services/api.service";
 import "./GenreNameListComponent.css"
 import {IGenre} from "@/src/models/GenreModel";
+import Link from "next/link";
 
 
 type propsType = {
@@ -17,7 +18,7 @@ const GenreNameListComponent: FC<propsType> = async ({genres}) => {
             {genres.map((value, index) => {
 
                 if (typeof value === "object" && value !== null) {
-                    return <span key={value.id + "-" + index}>{value.name}</span>;
+                    return <Link href={"/movies_of_genre/" + value.id} key={value.id + "-" + index}><span >{value.name}</span></Link>;
                 }
 
                 return <span key={value + "-" + index}>{genresDictionary[value] || "Unknown"}</span>;
