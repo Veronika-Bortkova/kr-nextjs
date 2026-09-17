3a2a8b0ef38e267a6942836e481bbdee427d43b1
Veronika-Bortkova <shishiga79@gmail.com>
2026-09-17 12:48:06 +0300

    finish+

--- src/app/(pablic)/MovieSearch/page.tsx
+++ src/app/(pablic)/MovieSearch/page.tsx
@@ -1,7 +1,9 @@
 
 import {redirect} from "next/navigation";
+import MoviesCardPlaceholderPage from "@/src/app/(pablic)/moviesCard/page";
 
 
 const MovieSearchdPlaceholderPage = async () => {
     redirect("/");
 };
+export default MovieSearchdPlaceholderPage;
